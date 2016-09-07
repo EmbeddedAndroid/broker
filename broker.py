00155c4e3815c305891193b3a5becfa8b5669a09
Tyler Baker <tyler.baker@linaro.org>
2016-09-07 07:18:32 -0700

    Remove unused imports and allow broker to publish

--- broker.py
+++ broker.py
@@ -1,21 +1,21 @@
-import time
 import zmq
-import random
 
 def broker():
     context = zmq.Context()
     # recieve work
     broker_receiver = context.socket(zmq.PULL)
-    broker_receiver.bind("tcp://0.0.0.0:5555")
-    # send work
-    #broker_sender = context.socket(zmq.PUSH)
-    #broker_sender.connect("tcp://127.0.0.1:5558")
+    broker_receiver.bind("tcp://*:5555")
+    # publish work
+    broker_publisher = context.socket(zmq.PUB)
+    broker_publisher.bind("tcp://*:5556")
     
     while True:
         work = broker_receiver.recv_json()
         if 'message' in work and 'id' in work:
             print 'Received Message: %s from %s' % (work['message'], work['id'])
+            broker_publisher.send(work['id'], work['message'])
         if 'command' in work:
             print 'Command Received'
 
-broker()
+if __name__ == '__main__':
+    broker()
